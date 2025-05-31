f9cf08faa556d9b30867145fb5db62d519f28ad1
Mohammed Rahman <26rahman2004@gmail.com>
2025-05-31 19:48:58 +0530

    ascending_order_array.java

--- ascending_order_array.java
+++ ascending_order_array.java
@@ -1,6 +1,9 @@
+//Write a program to sort the array in ascending order.
+import java.util.Arrays;
 public class ascending_order_array {
-    public static void main (String args[]){
-        int arr[] = {2, 5, 1, 7, 4};
-        
+    public static void main(String[] args) {
+        int[] arr = { 1, 4, 2, 3, -9, 5, 0 };
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
     }
 }
