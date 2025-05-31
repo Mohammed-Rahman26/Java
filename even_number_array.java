3d2d1cced417d1cd717b3d8295505d6854244e46
Mohammed Rahman <26rahman2004@gmail.com>
2025-05-31 19:52:43 +0530

    even_number_array.java

--- even_number_array.java
+++ even_number_array.java
@@ -1,3 +1,4 @@
+//Write a program to print even numbers from an array.
 public class even_number_array {
     public static void main (String [] args){
         int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
