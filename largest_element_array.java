cf52e0eb1b0675c00cf5ed84f2d8bca130e89e7f
Mohammed Rahman <26rahman2004@gmail.com>
2025-06-01 11:52:53 +0530

    largest_element_array.java

--- largest_element_array.java
+++ largest_element_array.java
@@ -1,3 +1,4 @@
+//Write a program to find the largest element from an array.
 public class largest_element_array {
     public static void main(String[] args){
         int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
