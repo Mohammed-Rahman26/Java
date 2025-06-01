ab21ab9af95b7f72a9ede0d617c54da0f4eb612d
Mohammed Rahman <26rahman2004@gmail.com>
2025-06-01 12:04:09 +0530

    marks.java

--- marks.java
+++ marks.java
@@ -1,4 +1,4 @@
-//Write a program to understand the nested loop to print marks dn rank achieved.
+//Write a program to understand the nested loop to print marks and rank achieved.
 import java.util.Scanner;
 public class marks {
     public static void main(String[] args){
