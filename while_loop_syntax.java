06df160fe29b0a7ac9c6dd019cf3c3cfe14673fa
Mohammed Rahman <26rahman2004@gmail.com>
2025-06-02 14:24:56 +0530

    while_loop_syntax.java

--- while_loop_syntax.java
+++ while_loop_syntax.java
@@ -1,3 +1,4 @@
+//Write a program to understand the syntax of while loop.
 public class while_loop_syntax {
     public static void main(String[] args){
         System.out.println("Numbers from 1 to 10 are :");
