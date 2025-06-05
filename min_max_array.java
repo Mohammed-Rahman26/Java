d0f3fc76547e10d7ac03bc0cc216ed2ea82013db
Mohammed Rahman <26rahman2004@gmail.com>
2025-06-05 10:20:12 +0530

    min_max_array.java

---
+++ min_max_array.java
@@ -0,0 +1,22 @@
+//Write a program to find the minimum and maximum element in an array.
+import java.util.Scanner;
+public class min_max_array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        int max = arr[0], min = arr[0];
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > max) max = arr[i];
+            if (arr[i] < min) min = arr[i];
+        }
+
+        System.out.println("Max = " + max);
+        System.out.println("Min = " + min);
+    }
+}
