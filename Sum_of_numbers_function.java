f17b26e97e38d65989d05d4ebfcc2f1579bf6517
Mohammed Rahman <26rahman2004@gmail.com>
2025-06-13 21:37:57 +0530

    sum_of_numbers_function.java

---
+++ Sum_of_numbers_function.java
@@ -0,0 +1,9 @@
+//Write a program to print sum of two numbers using function.
+public class SumFunction {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Sum = " + add(5, 3));
+    }
+}
